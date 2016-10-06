d8fe3175be58c0822e0d73bf22558633b02f262f
Hemendra Singh <HemendraSingh@Hemendras-Mac-mini.local>
2016-10-06 15:12:34 +0530

    final changes in table view animation

--- playFootBall/UIViewAnimationWithTableViewVC.swift
+++ playFootBall/UIViewAnimationWithTableViewVC.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class UIViewAnimationWithTableViewVC: UIViewController, UITableViewDelegate,UITableViewDataSource {
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var uiViewAnimationTblView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,10 +32,24 @@ class UIViewAnimationWithTableViewVC: UIViewController, UITableViewDelegate,UITa
     }
     
     func cellBtnAction(_ sender: UIButton) {
-        let cell = sender.superview?.superview?.superview?.superview as! UIViewAnimationTableCell
+        // Method (1) for getting table cell using superview of sender
+        
+       /* let cell = sender.superview?.superview?.superview?.superview as! UIViewAnimationTableCell
         let  myConstant = -60;
-        cell.rightViewMargin.constant = CGFloat(myConstant);
-        cell.leftViewMargin.constant = CGFloat(myConstant);
+        cell.rightViewMargin.constant = CGFloat(myConstant)
+        cell.leftViewMargin.constant = CGFloat(myConstant)
+        UIView.animate(withDuration: 0.4, animations: {
+            cell.layoutIfNeeded()
+            }, completion: {finished in })*/
+        
+        
+        // Method(2) for getting table cell using CGPoint
+        let  myConstant = -60
+        let point : CGPoint = sender.convert(CGPoint.zero, to:uiViewAnimationTblView)
+        let indexPath = uiViewAnimationTblView.indexPathForRow(at: point)
+        let cell = uiViewAnimationTblView.cellForRow(at: indexPath!) as! UIViewAnimationTableCell
+        cell.rightViewMargin.constant = CGFloat(myConstant)
+        cell.leftViewMargin.constant = CGFloat(myConstant)
         UIView.animate(withDuration: 0.4, animations: {
             cell.layoutIfNeeded()
             }, completion: {finished in })
